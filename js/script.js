c53c6f8c0334ce090ec627dc0ac91d3648579b8d
Elwin Ung <elwin.ung@gmail.com>
2020-09-25 11:29:17 -0400

    Upated time slot's color and added clear button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var index = 0;
 var time = 8;
+var tableTime = 8;
 
 var descriptList = localStorage.descriptList; 
 
-//Checking if the timeslot is past/present/future
-function deterColor(){
-    var result;
+//Checking if the timeslot is past/present/future and return the result
+function deterColor(time){
+    if (moment().hour() - time == 0) var result = "present";
+    else if (moment().hour() - time < 0) var result = "future";
+    else var result = "past";
     return result;
 }
 
@@ -16,13 +19,15 @@ function saveToLocal(event){
         //In case the icon is clicked
         if(event.target.matches("button")) indexList = event.target.value;
         else indexList = event.target.parentElement.value;
-        
+
         //Updating the description with its respective index.
         descriptList[indexList] = $(`#slot${indexList}`).val();
     }
+    //Updating the description in localStorage
     localStorage.descriptList = JSON.stringify(descriptList);
 }
 
+//Load description
 function writeDescript(){
     for (let i = 0; i < descriptList.length; i++){
         if(descriptList[i] != null){
@@ -33,20 +38,19 @@ function writeDescript(){
 
 //Generate timetable 
 function makeTable(){
-    var time2;
-    var color = deterColor();
-
-    if(time > 12){
-        time = 1;
-        time2 = "PM";
+    var tableTime2;
+    var color = deterColor(time);
+    if(tableTime > 12){
+        tableTime = 1;
+        tableTime2 = "PM";
     } else if (time < 12){
-        time2 = "AM";
-    } else time2 = "PM";
+        tableTime2 = "AM";
+    } else tableTime2 = "PM";
 
     $(".container").append(`
         <div class="input-group">
             <div class="input-group-prepend time-block">
-                <label class="input-group-text">${time}${time2}</label>
+                <label class="input-group-text">${tableTime}${tableTime2}</label>
             </div>
             <textarea class="form-control description ${color}" id="slot${index}"></textarea>
             <div class="input-group-append">
@@ -56,7 +60,19 @@ function makeTable(){
     `);
 
     index++;
+    tableTime++;
     time++;
+}
+
+//Clear schedule when "Clear schedule" is pressed
+function clearSchedule(event){
+    //Checking if the actual button is pressed
+    if(!event.target.matches("button")) return;
+
+    //Made a blank descriptionList and update its localStorage copy before reloading the page
+    descriptList = [];
+    localStorage.descriptList = JSON.stringify(descriptList);
+    location.reload();
 }
 
 $(document).ready(function(){
@@ -80,4 +96,5 @@ $(document).ready(function(){
     }, 1000);
 
     $(".saveBtn").on("click", saveToLocal);
+    $("#clearBtn").on("click", clearSchedule);
 });
